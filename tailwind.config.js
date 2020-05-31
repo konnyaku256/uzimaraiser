5fea52b6e536cb17ad5a93041954af9de564b922
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2020-05-31 15:59:23 +0900

    :sparkles: Add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "uzimaru-black": "#3C3C3C",
+        "uzimaru-gray": "#5A5A5A",
+        "uzimaru-green": "#199861"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
